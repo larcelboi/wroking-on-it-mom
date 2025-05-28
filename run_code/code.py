0e12107f67d93169b9d205751aa8908bd14a5184
larcel <2355315@cegepoutaouais.qc.ca>
2025-05-28 10:12:00 -0400

    changing ui

--- run_code/code.py
+++ run_code/code.py
@@ -1,19 +1,27 @@
 import sys
 
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtWidgets import QMainWindow, QApplication, QWidget
 from ui_main_app_code import Ui_Form
 
-class Program(QMainWindow):
+class Program(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.erreur_sign_nom.hide()
+        self.ui.erruer_sign_password2.hide()
+        self.ui.erreur_sign_password1.hide()
 
+        self.ui.bout_sign_up.clicked.connect(self.sign_up)
+        self.ui.bout_log_in.clicked.connect(self.log_in)
 
+    def sign_up(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
 
-
+    def log_in(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
 
 
 
